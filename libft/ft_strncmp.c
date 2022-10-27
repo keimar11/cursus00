1777add085b5792f1e6b760fb8263601dc59a45a
cui <tkeito36@gmail.com>
2022-10-27 14:42:27 +0100

    edit strncmp

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -20,8 +20,6 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (i != n)
 	{
 		if (s1[i] != s2[i])
-			if (s2[i] < 0)
-				return (-(256 - s1[i] - s2[i]));
 			return (s1[i] - s2[i]);
 		i++;
 	}
