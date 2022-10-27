b094d4b204d377f6492e677b6c5672d08f689331
cui <tkeito36@gmail.com>
2022-10-27 10:50:23 +0100

    edit atoi

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -20,8 +20,21 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (i != n)
 	{
 		if (s1[i] != s2[i])
+			if (s2[i] < 0)
+				return (-(256 - s1[i] - s2[i]));
 			return (s1[i] - s2[i]);
 		i++;
 	}
 	return (0);
 }
+
+// [test 52] ASSERT_EQ_I failed: ("173") is not equal to expected ("-83"). func main at file srcs/test_ft_strncmp.c, line 34
+// [test 53] ASSERT_EQ_I failed: ("87") is not equal to expected ("-169"). func main at file srcs/test_ft_strncmp.c, line 36
+
+// char *cmp2 = calloc(10, 1);
+// strlcpy(cmp2, "libft", 10);
+// cmp2[5] = CHAR_MIN;
+// /* 52 */ ASSERT_EQ_I(ft_strncmp(str, cmp2, len), strncmp(str, cmp2, len));
+// cmp2[5] = -42;
+// /* 53 */ ASSERT_EQ_I(ft_strncmp(str, cmp2, len), strncmp(str, cmp2, len));
+// char i = 'f';
