61cd05ec423d69b0e6653abecf34e3a84c7adf9f
cui <tkeito36@gmail.com>
2022-10-28 09:32:58 +0100

    edit strncmp

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -16,12 +16,34 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	while (n-- > 0)
 	{
-		if (*(char *)s1 != *(char *)s2)
-			return (*(char *)s1 - *(char *)s2);
+		if (*(unsigned char *)s1 != *(unsigned char *)s2)
+			return (*(unsigned char *)s1 - *(unsigned char *)s2);
 		s1++;
 		s2++;
 	}
 	return (0);
 }
 
-// (unsigned char *) -> (char *)
+// int	main(void)
+// {
+// 	char	*cmp2;
+
+// 	cmp2 = calloc(sizeof(char), 10);
+// 	ft_strlcpy(cmp2, "libft", 10);
+// 	cmp2[5] = CHAR_MIN;
+// 	printf("%d\n", ft_strncmp("libft", cmp2, 10));
+// 	cmp2[5] = -41;
+// 	printf("%d\n", ft_strncmp("libft", cmp2, 10));
+// }
+// char1 = 128
+// char2 = 41
+// unsigned char = -128
+// unsigned char = -215
+
+// (unsigned char *) -> (char *)になるとs2に-文字が入った時対応できなくなる
+// 	cmp2[5] = CHAR_MIN;
+// 52/	ASSERT_EQ_I(ft_strncmp(str, cmp2, len), strncmp(str, cmp2, len));
+// 	cmp2[5] = -42;
+// 53/	ASSERT_EQ_I(ft_strncmp(str, cmp2, len), strncmp(str, cmp2, len));
+// [test 52] ASSERT_EQ_I failed: ("173") is not equal to expected ("-83"). func main at file srcs/test_ft_strncmp.c, line 34
+// [test 53] ASSERT_EQ_I failed: ("87") is not equal to expected ("-169"). func main at file srcs/test_ft_strncmp.c, line 36
