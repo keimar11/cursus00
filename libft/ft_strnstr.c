c3e9baf9e68b809037a8dc43f6c8da49cde43b43
cui <tkeito36@gmail.com>
2022-10-28 13:43:19 +0100

    edit strnstr

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -36,26 +36,3 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	}
 	return (0);
 }
-
-int	main(void)
-{
-	char *str = "libft-test-tokyo";
-	int len = strlen(str);
-	for (int i = 0; i < len; i++)
-	{
-	// ASSERT_EQ_PTR(ft_strnstr(str, "", i), strnstr(str, "", i));
-	// ASSERT_EQ_PTR(ft_strnstr(str, "libft-test-tokyo", i), strnstr(str, "libft-test-tokyo", i));
-	// ASSERT_EQ_PTR(ft_strnstr(str, "libft", i), strnstr(str, "libft", i));
-	// ASSERT_EQ_PTR(ft_strnstr(str, "test", i), strnstr(str, "test", i));
-	printf("%d: %s\n", i, ft_strnstr(str, "test", i));
-	printf("%d: %s\n", i, strnstr(str, "test", i));
-	// ASSERT_EQ_PTR(ft_strnstr(str, "libft~", i), strnstr(str, "libft~", i));
-	// ASSERT_EQ_PTR(ft_strnstr(str, "z", i), strnstr(str, "z", i));
-	}
-	// /* 113 */ ASSERT_EQ_PTR(ft_strnstr("", "hello", 5), strnstr("", "hello", 5));
-	// /* 114 */ ASSERT_EQ_PTR(ft_strnstr("", "", 0), strnstr("", "", 0));
-	// /* 115 */ ASSERT_EQ_PTR(ft_strnstr(NULL, "1", 0), strnstr(NULL, "1", 0));
-	// Segmentation Fault
-	// /* 116 */ ASSERT_EQ_PTR(ft_strnstr(NULL, "fake", 3), strnstr(NULL, "fake", 3));
-	// /* 116 */ ASSERT_EQ_PTR(ft_strnstr(NULL, "1", 1), ft_strnstr(NULL, "1", 1));
-}
