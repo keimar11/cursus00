ae3e60cd104332d9072a1868b5532d9006ff8c05
cui <tkeito36@gmail.com>
2022-11-03 15:28:02 +0100

    edit substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 21:41:43 by marvin            #+#    #+#             */
-/*   Updated: 2022/11/03 21:36:38 by marvin           ###   ########.fr       */
+/*   Updated: 2022/11/03 23:26:22 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,19 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
+	size_t	i;
 
 	if ((size_t)start > ft_strlen(s))
 		return (NULL);
 	sub = (char *)malloc(sizeof(char) * len + 1);
-	if (!sub)
+	if (sub == NULL)
 		return (NULL);
-	ft_strlcpy(sub, s + start, len);
+	i = 0;
+	while (i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub [i] = 0;
 	return (sub);
 }
-
-// #19 そもそもstartがsをオーバーしてたらコピーできない
