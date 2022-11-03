003893a7e81d0586e65b0b04534ca3d669d6ffca
cui <tkeito36@gmail.com>
2022-11-03 17:37:06 +0100

    edit strlcpy

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -11,19 +11,16 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 
-	printf("%s\n%s\n", dst, src);
 	if (size == 0)
 		return (ft_strlen ((char *)src));
 	i = 0;
 	while (i++ < (size - 1))
 		*dst = *src;
 	*dst = '\0';
-	printf("%s\n%s\n", dst, src);
 	return (ft_strlen ((char *)src));
 }
