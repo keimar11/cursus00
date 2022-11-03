f3097b995abbb8ca315353de5716e746107df8a1
cui <tkeito36@gmail.com>
2022-11-03 16:13:25 +0100

    strncmp

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -14,6 +14,8 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+	if (!s1)
+		return (*(unsigned char *)s1 - *(unsigned char *)s2);
 	while (*(unsigned char *)s1 != 0 && n-- > 0)
 	{
 		if (*(unsigned char *)s1 != *(unsigned char *)s2)
