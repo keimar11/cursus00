4371976e9e1d4b06b9d12884ba334f8ee262e413
cui <tkeito36@gmail.com>
2022-11-03 17:43:58 +0100

    edit strlcpy

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -12,22 +12,6 @@
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
-{
-	size_t	i;
-
-	if (size == 0)
-		return (ft_strlen ((char *)src));
-	i = 0;
-	while (i < (size - 1))
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = 0;
-	return (ft_strlen ((char *)src));
-}
-
 // size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 // {
 // 	size_t	i;
@@ -35,8 +19,24 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 // 	if (size == 0)
 // 		return (ft_strlen ((char *)src));
 // 	i = 0;
-// 	while (i++ < (size - 1))
-// 		*dst++ = *src++;
-// 	*dst = '\0';
+// 	while (i < (size - 1))
+// 	{
+// 		dst[i] = src[i];
+// 		i++;
+// 	}
+// 	dst[i] = 0;
 // 	return (ft_strlen ((char *)src));
 // }
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	src_l;
+
+	src_l = ft_strlen((char *)src);
+	if (size == 0)
+		return (src_l);
+	while (i++ < (size - 1))
+		*dst++ = *src++;
+	*dst = '\0';
+	return (src_l);
+}
