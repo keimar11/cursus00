cd8645d21594d6ca2531492adf483174ada1fbf1
cui <tkeito36@gmail.com>
2022-11-11 07:12:48 +0000

    edit itoa

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -5,57 +5,12 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/15 21:26:14 by marvin            #+#    #+#             */
-/*   Updated: 2022/11/02 22:24:01 by marvin           ###   ########.fr       */
+/*   Created: 2022/11/11 16:12:02 by marvin            #+#    #+#             */
+/*   Updated: 2022/11/11 16:12:02 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static char	*str_rev(char *str)
-{
-	char	*scr;
-	int		cnt;
-	int		i;
-
-	cnt = ft_strlen (str);
-	scr = (char *)malloc(sizeof(char) * (cnt + 1));
-	i = 0;
-	while (cnt-- > 0)
-	{
-		scr[i] = str[cnt];
-		i++;
-	}
-	scr[i] = 0;
-	return (scr);
-}
-
-static char	*str_fit(char *str, int n)
-{
-	int	i;
-	int	flag;
-
-	if (n == -2147483648)
-		return (str = "8463847412-");
-	i = 0;
-	flag = 0;
-	if (n == 0)
-		str[i++] = '0';
-	else if (n < 0)
-	{
-		n *= -1;
-		flag = 1;
-	}
-	while (n != 0)
-	{
-		str[i++] = '0' + (n % 10);
-		n /= 10;
-	}
-	if (flag == 1)
-		str[i++] = '-';
-	str[i] = 0;
-	return (str);
-}
 
 static int	str_cnt(int n)
 {
@@ -74,6 +29,39 @@ static int	str_cnt(int n)
 	return (cnt);
 }
 
+static char	*str_min_fit(char *str, int n)
+{
+	int	i;
+	char	*min;
+
+	i = 0;
+	min = "-2147483648";
+	while (*min != '\0')
+		str[i++] = *min++;
+	return (str);
+}
+
+static char	*str_fit(char *str, int n)
+{
+	int	i;
+
+	i = str_cnt(n);
+	if (n == 0)
+		str[0] = '0';
+	if (n < 0)
+	{
+		str[0] = '-';
+		n*= -1;
+	}
+	str[i--] = '\0';
+	while (n != 0)
+	{
+		str[i--] = '0' + (n % 10);
+		n /= 10;
+	}
+	return (str);
+}
+
 char	*ft_itoa(int n)
 {
 	char	*str;
@@ -81,13 +69,9 @@ char	*ft_itoa(int n)
 	str = (char *)malloc(sizeof(char) * str_cnt(n) + 1);
 	if (str == NULL)
 		return (NULL);
-	str = str_fit(str, n);
-	str = str_rev(str);
+	if (n == INT_MIN)
+		str = str_min_fit(str, n);
+	else
+		str = str_fit(str, n);
 	return (str);
 }
-
-// int main()
-// {
-// 	printf("%s\n", ft_itoa(-2147483648));
-// 	return (0);
-// }
