bc1fb66d78198ae22cb5c051c2c3f694cd532937
Keito Takahashi <keitakah@c1r13s8.42tokyo.jp>
2022-11-11 18:28:38 +0900

    arrange

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,52 +6,16 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/16 00:14:42 by marvin            #+#    #+#             */
-/*   Updated: 2022/11/02 22:26:23 by marvin           ###   ########.fr       */
+/*   Updated: 2022/11/11 18:25:04 by keitakah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	number(int a, char *arr, int i, int fd)
-{
-	int	b;
-
-	if (a < 0)
-	{
-		write(fd, "-", 1);
-		a = -a;
-	}
-	while (1)
-	{
-		b = a % 10;
-		a = a / 10;
-		arr[i] = b + '0';
-		i++;
-		if (a == 0)
-		{
-			break ;
-		}
-	}
-	i--;
-	while (i >= 0)
-	{
-		write (fd, &arr[i], 1);
-		i--;
-	}
-}
-
 void	ft_putnbr_fd(int n, int fd)
 {
-	int		a;
-	int		i;
-	char	arr[100];
+	char	*str;
 
-	a = n;
-	i = 0;
-	if (n == -2147483648)
-	{
-		write (fd, "-2147483648", 11);
-		return ;
-	}
-	number (a, arr, i, fd);
+	str = ft_itoa(n);
+	ft_putstr_fd(str, fd);
 }
